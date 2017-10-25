d5f5f4cf0aa1f88e725e6b9101eac2bb294820c7
Jay Zhou <jayzml@gmail.com>
2017-10-25 12:11:34 +0800

    新增device_logs表，保存每一条设备上传的日志信息

--- trunk/src/appserver/lib/device_mongo_dao.py
+++ trunk/src/appserver/lib/device_mongo_dao.py
@@ -121,3 +121,28 @@ class DeviceMongoDAO(MongoDAOBase):
                           upsert=True)
 
         yield self.submit(_callback)
+
+    @gen.coroutine
+    def add_device_log(self, **kwargs):
+        device_log = kwargs
+
+        def _callback(mongo_client, **kwargs):
+            tb = mongo_client[device_def.DEVICE_DATABASE][
+                device_def.DEVICE_LOGS_TB]
+
+            row = device_def.new_device_logs_row()
+            for (k, v) in device_log.items():
+                if row.has_key(k):
+                    row[k] = v
+                else:
+                    raise DeviceMongoDAOException(
+                        "Unknwon device log row column \"%s\"", k)
+
+            validate_ret, exp_col = device_def.validate_device_logs_row(row)
+            if not validate_ret:
+                raise DeviceMongoDAOException(
+                    "Validate device log row failed, invalid column \"%s\"",
+                    exp_col)
+            tb.insert_one(row)
+
+        yield self.submit(_callback)
