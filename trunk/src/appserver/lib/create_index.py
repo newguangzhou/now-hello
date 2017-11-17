26495c7f4d8d468d1e513995eebcb5db9e053bc1
Jay@test.svr <jayzml@gmail.com>
2017-11-17 10:31:17 +0800

    修改create_index

--- trunk/src/appserver/lib/create_index.py
+++ trunk/src/appserver/lib/create_index.py
@@ -9,49 +9,19 @@ from op_log_mongo_defines import *
 from pet_mongo_defines import *
 from user_mongo_defines import *
 from auth_mongo_defines import *
+from config import *
 
-class MongoMeta:
-    def __init__(self, *args, **kwargs):
-        self.host = "172.19.101.61"
-        self.port = 27017
-        self.username = None
-        self.passwd = None
-        self.repl_set_name = None
-        self.max_thread_count = 3
-
-        if kwargs.has_key("hosts"):
-            self.host = kwargs["hosts"]
-        if kwargs.has_key("port"):
-            self.port = kwargs["port"]
-
-        if kwargs.has_key("username"):
-            self.username = kwargs["username"]
-
-        if kwargs.has_key("passwd"):
-            self.passwd = kwargs["passwd"]
-
-        if kwargs.has_key("repl_set_name"):
-            self.repl_set_name = kwargs["repl_set_name"]
-
-        if kwargs.has_key("max_thread_count"):
-            self.max_thread_count = kwargs["max_thread_count"]
-
-hosts = "172.17.0.2:27018,172.17.0.2:27019,172.17.0.2:27020"
-hosts = "127.0.0.1:27018,127.0.0.1:27019,127.0.0.1:27020"
-default_meta = MongoMeta(hosts=hosts,
-                                      port=27020,
-                                      username="root",
-                                      passwd="mgdb8w34asdadat51!((",
-                                      repl_set_name="mongo_shard1")
+conf = loadJsonConfig("../configs/config.json")
+default_meta = conf["mongodb"]
 
 extra_args = {"w": 1, "j": True}
 
-mongo_client = pymongo.MongoClient(default_meta.host, replicaset=default_meta.repl_set_name,
+mongo_client = pymongo.MongoClient(default_meta["hosts"], default_meta["port"],
                                                          **extra_args)
 mongo_client.get_database("admin").authenticate(
-                default_meta.username,
-                default_meta.passwd,
-                mechanism='SCRAM-SHA-1')
+                default_meta["username"],
+                default_meta["passwd"],
+                mechanism="SCRAM-SHA-1")
 
 def create_auth_indexes():
     tb = mongo_client[AUTH_DATABASE][AUTH_STATUS_TB]
