8b44fb16ca328ec5a0404cb72bf5dcd0b608f35b
Jay@test.svr <jayzml@gmail.com>
2017-11-21 15:23:05 +0800

    增加测试框架

--- trunk/src/appserver/lib/test.py
+++ trunk/src/appserver/lib/test.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
 import sys
 sys.path.append("../")
-#from lib import sys
-from lib import config
+from lib.config import *
 from lib import utils
 from lib import pet_mongo_dao
+from lib.pet_dao import PetDAO
+from tornado import ioloop, gen
+from configs.mongo_config import MongoConfig2
 
-def testfun():
-        conf = loadJsonConfig("../config/config.json")
+@gen.coroutine
+def testfun(uid):
+        conf = loadJsonConfig("../configs/config.json")
         mongo_conf = MongoConfig2(conf["mongodb"])
         pet_dao=PetDAO.new(mongo_meta=mongo_conf.pet_mongo_meta)
-        tornado.ioloop.IOLoop.instance().start()
-        count = yield pet_dao.get_pet_count()
-        return count
+        count = yield pet_dao.get_pet_count(uid)
+        print "count:",count
+
 if __name__ == '__main__':
-    print "main"
+    print "main", sys.argv[1]
     try:
-        print "fun return:",testfun()
+        ioloop.IOLoop.instance().run_sync(lambda: testfun(int(sys.argv[1])))
     except Exception,e :
         print e
